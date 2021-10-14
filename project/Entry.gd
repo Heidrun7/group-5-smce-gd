d5196155303a07e42f544307ccc38d57b6925e6a
Heidrun Heidarsdottir <heidrunh17@ru.is>
2021-10-14 12:35:23 +0200

    Fix error msg comment based on Ruthgers comment

--- project/Entry.gd
+++ project/Entry.gd
@@ -92,7 +92,7 @@ func _continue():
 		return _error("No Main Scene")
 	get_tree().change_scene_to(main_scene)
 
-# Report if log file successful or not
+# Sets the error string to the users clipboard
 func _error(message: String) -> void:
 	var file: File = File.new()
 	var result = file.open("user://logs/godot.log", File.READ)
