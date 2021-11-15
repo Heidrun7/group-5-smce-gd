f2b1845b0bd46e1ece97268a9d1c6baeed7a35c2
Pontus Lindblom <ponlind@student.chalmers.se>
2021-11-15 11:07:49 +0100

    Clean code in HelpPane.gd

--- project/src/ui/hud/HelpPane.gd
+++ project/src/ui/hud/HelpPane.gd
@@ -1,4 +1,4 @@
-#  HelpPane.gd
+# HelpPane.gd
 
 extends PanelContainer
 
@@ -10,6 +10,4 @@ onready var toggle_btn: Button = $VBoxContainer/MarginContainerBottom/Toggle
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	toggle_btn.connect("pressed", self, "emit_signal", ["toggled"])
-	pass # Replace with function body.
-
-
+	pass
