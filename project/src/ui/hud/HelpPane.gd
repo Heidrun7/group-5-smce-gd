a462473d66790bb96010102bd2e50bfd1b71f341
birgittafeldis <felds@student.chalmers.se>
2021-11-15 10:24:56 +0100

    allow for closing the help pane once opened

--- project/src/ui/hud/HelpPane.gd
+++ project/src/ui/hud/HelpPane.gd
@@ -1,8 +1,15 @@
-extends Node
+#  HelpPane.gd
+
+extends PanelContainer
 
 signal toggled
+
+onready var toggle_btn: Button = $VBoxContainer/MarginContainerBottom/Toggle
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	toggle_btn.connect("pressed", self, "emit_signal", ["toggled"])
 	pass # Replace with function body.
+
+
